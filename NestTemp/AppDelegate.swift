14bfe7a3b4b1e03df9f2db842e98458c8ae00a4f
Matt Lampe <matt@lampe.io>
2017-05-08 13:27:38 -0400

    Initial commit

---
+++ NestTemp/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  NestTemp
+//
+//  Created by Matt Lampe on 5/8/17.
+//  Copyright © 2017 Matt Lampe. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
